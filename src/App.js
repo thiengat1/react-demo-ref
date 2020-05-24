6bf1c3c2b62c222d574eaed15380b746ddece7db
thienga <thiengat1@gmail.com>
2020-05-23 22:21:32 -0700

    demo ref

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.inputElement=React.createRef();
+
+  }
+  componentDidMount(){
+    debugger;
+    this.inputElement.current.focus()
+  }
+  render(){
+    return (
+      <div className="App">
+        <input type="text" ref={this.inputElement}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
